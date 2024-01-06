00d005f210ce14c79452dc498672bcaefda9f2f9
T0nci <138928016+T0nci@users.noreply.github.com>
2024-01-06 22:11:24 +0100

    feat: Add drawing effect
    Added drawing effect using 'mouseenter' and not 'mouseover' because
    the former only triggers the event once and we only need it once, while
    the latter triggers it multiple times if moving over it and can be
    maliciously abused to cause lag.
    
    Also updated the background color through adding a special class so I
    can exercise more using the classList property and avoid using inline
    style.
    
    Transferred the logic for populating the grid into a function, so I
    can use it in a later step instead of rewriting old code and used it
    to add event listeners instead of writing more unnecessary code.

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-const grid = document.querySelector(".grid");
-let size = 16;
+function populateGrid(size) {
+    for (let i = 0; i < size * size; i++) {
+        const div = document.createElement("div");
 
-for (let i = 0; i < size * size; i++) {
-    const div = document.createElement("div");
-    div.style.width = `${parseInt(window.getComputedStyle(grid).width) / 
-    size}px`;
-    div.style.height = `${parseInt(window.getComputedStyle(grid).height) / 
-    size}px`;
-    grid.appendChild(div);
+        div.style.width = `${parseInt(window.getComputedStyle(grid).width) / 
+        size}px`;
+        div.style.height = `${parseInt(window.getComputedStyle(grid).height) / 
+        size}px`;
+
+        grid.appendChild(div);
+
+        div.addEventListener("mouseenter", () => {
+            div.classList.add("black");
+        });
+    }
 }
+
+
+const grid = document.querySelector(".grid");
+
+let startingSize = 16;
+
+populateGrid(startingSize);
