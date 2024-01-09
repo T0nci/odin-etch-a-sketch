cb3b2877f7fef758a0b05b4104a50f165daf2a81
T0nci <138928016+T0nci@users.noreply.github.com>
2024-01-09 11:36:01 +0100

    feat: Add random RGB painting effect
    Added 'Extra Credit' exercise 1. Made it possible through mutual
    exclusiveness of buttons. If one button is active another cannot be.
    
    Also the populateGrid event listeners now always check if a button is
    active or not. Instead of adding event listeners on every div for every
    button, I used the same event listeners from the start.
    
    Started the darkening exercise but didn't finish it.

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+const grid = document.querySelector(".grid");
+let startingSize = 16;
+populateGrid(startingSize);
+
+const btnChangeSize = document.querySelector("#change-size");
+btnChangeSize.addEventListener("click", () => {
+    newSize = parseInt(prompt("Enter a whole number from 1 to 100:", 16));
+
+    if (isNaN(newSize) || newSize < 1 || newSize > 100) {
+        console.log("Invalid number! Launching defensive measures");
+        populateGrid(startingSize);
+        btnChangeSize.textContent = "Change Size: 16x16";
+    } else {
+        populateGrid(newSize);
+        btnChangeSize.textContent = `Change Size: ${newSize}x${newSize}`;
+    }
+});
+
+const arrayButtons = [];
+const btnRandomRGB = document.querySelector("#randomRGB");
+const btnDarken = document.querySelector("#darken");
+arrayButtons.push(btnRandomRGB, btnDarken);
+arrayButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        if (btn.style.backgroundColor === "lightsalmon") {
+            btn.style.backgroundColor = "";
+        } else {
+            arrayButtons.forEach((button) => {
+                if (button.style.backgroundColor === "lightsalmon") {
+                    button.style.backgroundColor = "";
+                }
+            });
+            btn.style.backgroundColor = "lightsalmon";
+        }
+    });
+});
+
+
 function populateGrid(size) {
     grid.innerHTML = ""; // Clear the grid if it's not empty
 
@@ -12,27 +50,23 @@ function populateGrid(size) {
         grid.appendChild(div);
 
         div.addEventListener("mouseenter", () => {
-            div.classList.add("black");
+            if (btnDarken.style.backgroundColor === "lightsalmon") {
+                div.style.backgroundColor = "gray";
+            } else if (btnRandomRGB.style.backgroundColor === "lightsalmon") {
+                div.style.backgroundColor = getRGBColor();
+            } else {
+                div.style.backgroundColor = "black";
+            }
         });
     }
 }
 
 
-const grid = document.querySelector(".grid");
-const btnChangeSize = document.querySelector("#change-size");
+function getRandomRGB() {
+    return Math.floor(Math.random() * 256); // from 0 to 255
+}
 
-let startingSize = 16;
-populateGrid(startingSize);
 
-btnChangeSize.addEventListener("click", () => {
-    newSize = parseInt(prompt("Enter a whole number from 1 to 100:", 16));
-
-    if (isNaN(newSize) || newSize < 1 || newSize > 100) {
-        console.log("Invalid number! Launching defensive measures");
-        populateGrid(startingSize);
-        btnChangeSize.textContent = "Change Size: 16x16";
-    } else {
-        populateGrid(newSize);
-        btnChangeSize.textContent = `Change Size: ${newSize}x${newSize}`;
-    }
-});
+function getRGBColor() {
+    return `rgb(${getRandomRGB()} ${getRandomRGB()} ${getRandomRGB()})`;
+}
